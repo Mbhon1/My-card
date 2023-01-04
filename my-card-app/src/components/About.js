0861e0c903fde60f96c1b253c1d80237fdb5d929
Mbhoni Baloyi <mbhoni@proton.me>
2023-01-04 17:32:18 +0200

    Update About.js

--- my-card-app/src/components/About.js
+++ my-card-app/src/components/About.js
@@ -5,9 +5,14 @@ const About = () => {
         <div className="about--container">
             <h4 className="about--title">About</h4>
             <p className="about--desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Nisi excepturi incidunt ut qui! Illum, magnam tenetur veniam, 
-                laboriosam cum aliquam pariatur ratione alias porro vero nihil.
+                In my free time, I enjoy exploring new technologies and 
+                staying up-to-date with the latest developments in 
+                web development. 
+                I also enjoy participating in online coding challenges a
+                nd working on personal projects to expand my skillset. 
+                In addition to my interest in technology, 
+                I enjoy video gaming and experiencing different cultures, 
+                as well as spending time with my family and friends.
             </p>
         </div>
     )
