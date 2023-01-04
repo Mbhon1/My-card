b71080c6b2e62f29b8d6acc5b503f8e781053e93
Mbhoni Baloyi <mbhoni@proton.me>
2023-01-04 17:33:18 +0200

    Update About.js

--- my-card-app/src/components/About.js
+++ my-card-app/src/components/About.js
@@ -5,14 +5,15 @@ const About = () => {
         <div className="about--container">
             <h4 className="about--title">About</h4>
             <p className="about--desc">
-                In my free time, I enjoy exploring new technologies and 
-                staying up-to-date with the latest developments in 
-                web development. 
-                I also enjoy participating in online coding challenges a
-                nd working on personal projects to expand my skillset. 
-                In addition to my interest in technology, 
-                I enjoy video gaming and experiencing different cultures, 
-                as well as spending time with my family and friends.
+                I am a web developer with a background in networking. 
+                I have always been interested in technology and have a 
+                strong foundation in computer systems. Recently, 
+                I have been focusing on learning web development 
+                and have been enjoying the challenge of building 
+                dynamic and interactive websites. 
+                I am constantly striving to improve 
+                my skills and stay up-to-date with the 
+                latest technologies in the field.
             </p>
         </div>
     )
